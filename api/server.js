28e13d968640f6ce63f444ef06ed7c2727d4bef5
davekaj <kajpustd@gmail.com>
2017-06-09 13:36:34 -0400

    fixServer

--- api/server.js
+++ api/server.js
@@ -16,10 +16,10 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use(express.static(__dirname + './../build'));
+app.use(express.static(__dirname + '../../build'));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve((__dirname + './../build/index.html')));
+    res.sendFile(path.resolve((__dirname + '../../build/index.html')));
 });
 
 app.listen(PORT, () => {
